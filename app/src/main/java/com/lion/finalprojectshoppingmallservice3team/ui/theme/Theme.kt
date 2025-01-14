ef175343c3c7c98e59c334025eb0041bf5afb7fa
jaeminKimPea <kg8480@gmail.com>
2025-01-14 17:32:21 +0900

    Commit

--- app/src/main/java/com/lion/finalprojectshoppingmallservice3team/ui/theme/Theme.kt
+++ app/src/main/java/com/lion/finalprojectshoppingmallservice3team/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.lion.finalprojectshoppingmallservice3team.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
