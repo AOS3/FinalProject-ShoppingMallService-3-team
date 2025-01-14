f0d7915a4d0ac3a1819197133d54f04006bd2ea0
jaeminKimPea <kg8480@gmail.com>
2025-01-14 17:28:37 +0900

    Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.lion.finalprojectshoppingmallservice3team"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.lion.finalprojectshoppingmallservice3team"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
