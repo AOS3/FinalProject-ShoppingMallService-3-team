da0fc0c4cd0c4a3fd4f8c3b6d355e3ba1d86ee89
J <jeh200223@gmail.com>
2025-01-15 11:13:02 +0900

    hiltViewmodel 작업

---
+++ app/src/main/java/com/lion/finalprojectshoppingmallservice3team/ShoppingApplication.kt
@@ -0,0 +1,8 @@
+package com.lion.finalprojectshoppingmallservice3team
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ShoppingApplication : Application(){
+}
