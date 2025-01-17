b1cde4e5e2cfb0876922a5fd275f732b639a4c02
J <jeh200223@gmail.com>
2025-01-17 11:27:39 +0900

    이미지 비트맵 컴포넌트 수정

--- app/src/main/java/com/lion/finalprojectshoppingmallservice3team/Component/LikeLionImageBitmap.kt
+++ app/src/main/java/com/lion/finalprojectshoppingmallservice3team/Component/LikeLionImageBitmap.kt
@@ -1,13 +1,10 @@
-package com.lion.a02_boardcloneproject.component
+package com.lion.finalprojectshoppingmallservice3team.Component
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 
 
