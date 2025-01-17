06b94441ffa255e626b4847e5a04174c76796d6c
J <jeh200223@gmail.com>
2025-01-17 11:25:45 +0900

    다이얼로그 컴포넌트 수정

--- app/src/main/java/com/lion/finalprojectshoppingmallservice3team/Component/LikeLionAlertDialog.kt
+++ app/src/main/java/com/lion/finalprojectshoppingmallservice3team/Component/LikeLionAlertDialog.kt
@@ -1,16 +1,20 @@
-package com.lion.a02_boardcloneproject.component
+package com.lion.finalprojectshoppingmallservice3team.Component
 
-import android.util.Log
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.lion.finalprojectshoppingmallservice3team.ui.theme.MainColor
 
 @Composable
 fun LikeLionAlertDialog(
@@ -24,6 +28,17 @@ fun LikeLionAlertDialog(
     dismissButtonOnClick : () -> Unit = {
         showDialogState.value = false
     },
+    confirmcontainerColor: Color = MainColor,
+    confirmcontentColor: Color = Color.White,
+    confirmbuttonModifier: Modifier = Modifier,
+    dismisscontainerColor: Color = Color.Transparent,
+    dismisscontentColor: Color = Color.Black,
+    dismissbuttonModifier: Modifier = Modifier, // 기본값 설정
+    dismissBorder: BorderStroke = BorderStroke(0.dp, Color.Transparent),
+    titleAlign : TextAlign? = null,
+    textModifier: Modifier = Modifier,
+    titleModifier: Modifier = Modifier,
+    textAlign: TextAlign? = null,
     icon : ImageVector? = null,
     title : String? = null,
     text : String? = null,
@@ -31,35 +46,38 @@ fun LikeLionAlertDialog(
     if(showDialogState.value){
         AlertDialog(
             modifier = Modifier.fillMaxWidth(),
-            // 다이얼로그가 닫히는 사건이 발생했을 때
             onDismissRequest = {
                 showDialogState.value = false
             },
-            // 확인 버튼
+            // 확인 및 취소 버튼을 한 줄에 배치
             confirmButton = {
-                TextButton(
-                    onClick = {
-                        confirmButtonOnClick()
-                    }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly // 동일 간격 배치
                 ) {
-                    Text(text = confirmButtonTitle)
-                }
-            },
-            // 취소 버튼
-            dismissButton = if(dismissButtonTitle != null){
-                {
-                    TextButton(
-                        onClick = {
-                            dismissButtonOnClick()
-                        }
-                    ) {
-                        Text(text = dismissButtonTitle)
+                    // 취소 버튼
+                    if (dismissButtonTitle != null) {
+                        LikeLionFilledButton(
+                            modifier = dismissbuttonModifier,
+                            containerColor = dismisscontainerColor,
+                            contentColor = dismisscontentColor,
+                            text = dismissButtonTitle,
+                            border = dismissBorder,
+                            onClick = dismissButtonOnClick
+                        )
                     }
+                    // 확인 버튼
+                    LikeLionFilledButton(
+                        modifier = confirmbuttonModifier,
+                        containerColor = confirmcontainerColor,
+                        contentColor = confirmcontentColor,
+                        text = confirmButtonTitle,
+                        onClick = confirmButtonOnClick
+                    )
                 }
-            } else {
-                null
             },
-            icon = if(icon != null){
+            icon = if (icon != null) {
                 {
                     Icon(
                         imageVector = icon,
@@ -69,23 +87,28 @@ fun LikeLionAlertDialog(
             } else {
                 null
             },
-            title = if(title != null){
+            title = if (title != null) {
                 {
                     Text(
-                        text= title,
+                        text = title,
+                        textAlign = titleAlign,
+                        modifier = titleModifier
                     )
                 }
             } else {
                 null
             },
-            text = if(text != null){
+            text = if (text != null) {
                 {
-                    Text(text = text)
+                    Text(
+                        text = text,
+                        textAlign = textAlign,
+                        modifier = textModifier
+                    )
                 }
             } else {
                 null
             },
-
         )
     }
 }
