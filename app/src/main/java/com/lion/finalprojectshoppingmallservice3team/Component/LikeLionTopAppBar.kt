110dca3f48e6c680160ecd5c8fc785cef78905ca
J <jeh200223@gmail.com>
2025-01-17 11:29:30 +0900

    탑 앱바 컴포넌트 수정

--- app/src/main/java/com/lion/finalprojectshoppingmallservice3team/Component/LikeLionTopAppBar.kt
+++ app/src/main/java/com/lion/finalprojectshoppingmallservice3team/Component/LikeLionTopAppBar.kt
@@ -1,20 +1,21 @@
-package com.lion.a02_boardcloneproject.component
+package com.lion.finalprojectshoppingmallservice3team.Component
 
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LikeLionTopAppBar(
     title:String = "",
+    backColor:Color,
     navigationIconImage:ImageVector? = null,
     navigationIconOnClick:() -> Unit = {},
     menuItems : @Composable RowScope.() -> Unit = {},
@@ -24,6 +25,7 @@ fun LikeLionTopAppBar(
         title = {
             Text(text = title)
         },
+        colors = TopAppBarDefaults.topAppBarColors(backColor),
         // 네비게이션 아이콘
         navigationIcon = if(navigationIconImage == null){
             {}
