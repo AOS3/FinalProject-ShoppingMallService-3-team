6123da21d1d86a8647c7aa83e3f23929ed1fbedc
J <jeh200223@gmail.com>
2025-01-21 16:46:45 +0900

    탭 타이틀만 출력되게 수정

--- app/src/main/java/com/lion/finalprojectshoppingmallservice3team/Component/LikeLionFixedTabs.kt
+++ app/src/main/java/com/lion/finalprojectshoppingmallservice3team/Component/LikeLionFixedTabs.kt
@@ -11,10 +11,11 @@ import com.lion.finalprojectshoppingmallservice3team.ui.theme.MainColor
 
 @Composable
 fun LikeLionFixedTabs(
-    tabTitleWithCounts: List<Pair<String, Int>>,
+    tabTitleWithCounts: List<String>,
     selectedTabIndex: Int,
     onTabSelected: (Int) -> Unit,
     modifier: Modifier = Modifier,
+    TabModifier: Modifier = Modifier,
     divider: @Composable () -> Unit = {},
 ) {
     TabRow(
@@ -23,16 +24,17 @@ fun LikeLionFixedTabs(
         modifier = modifier.fillMaxWidth(),
         divider = divider
     ) {
-        tabTitleWithCounts.forEachIndexed { index, (title, count) ->
+        tabTitleWithCounts.forEachIndexed { index, title ->
             Tab(
                 selected = selectedTabIndex == index,
                 onClick = { onTabSelected(index) },
                 text = {
                     Text(
-                        text = "$title $count",
+                        text = title,
                         color = if (selectedTabIndex == index) MainColor else Color(0xFF49454F),
                     )
                 },
+                modifier = TabModifier
             )
         }
     }
